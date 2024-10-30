d050f0d50402884e27869be7ed4c6dd13da825cc
imhwarang <imhwarang4@naver.com>
2024-10-31 00:08:13 +0900

    chore: 스타일 파일 세팅

--- week3/assignment/week3-assignment/src/App.jsx
+++ week3/assignment/week3-assignment/src/App.jsx
@@ -1,5 +1,15 @@
+import { Global, ThemeProvider } from "@emotion/react";
+import GlobalStyle from "./styles/global";
+import { Theme } from "./styles/theme";
+
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <ThemeProvider theme={Theme}>
+        <Global styles={GlobalStyle} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ week3/assignment/week3-assignment/src/styles/theme.js
@@ -0,0 +1,18 @@
+const color = {
+  black: "#17171a",
+  white: "#fff",
+
+  lightgray: "#f4f4f4",
+  lightgray2: "#c6cad4",
+
+  purple1: "#5451FF",
+  purple2: "#7774FF",
+  purple3: "#9997FF",
+  purple4: "#BBB9FF",
+  purple5: "#DDDCFF",
+  purple6: "#EEEEFF",
+};
+
+export const Theme = {
+  color,
+};
